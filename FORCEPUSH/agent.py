98f843dd216f563d79f9c0dcf7eefa01688a3114
root <root@localhost.localdomain>
2020-06-21 22:36:07 -0400

    first commit

---
+++ FORCEPUSH/agent.py
@@ -0,0 +1,24 @@
+import socket
+import sys
+import json
+
+HOST, PORT = "localhost", 1776
+
+m =b'{"id": 1, "name": "FORCEPUSH"}'
+
+# Create a socket (SOCK_STREAM means a TCP socket)
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    # Connect to server and send data
+    sock.connect((HOST, PORT))
+    sock.sendall(m)
+
+
+    # Receive data from the server and shut down
+    received = sock.recv(1024)
+finally:
+    sock.close()
+
+print("Sent:     {}".format(data))
+print("Received: {}".format(received))
---
+++ FORCEPUSH/timer.py
@@ -0,0 +1,8 @@
+import time
+import agent
+
+timer = 30 # 30 seconds
+while timer > 0:
+    time.sleep(0.985)
+    timer -= 1
+agent()
