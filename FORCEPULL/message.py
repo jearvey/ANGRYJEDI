c15fd9cf0b83cf33aa3d5644541d020c990d854e
root <root@localhost.localdomain>
2020-09-20 18:49:25 -0400

    init all done and working. Diff types of messages are working and proccessed. Need to work on timer

--- FORCEPULL/message.py
+++ FORCEPULL/message.py
@@ -1,5 +1,6 @@
 import json
 
+
 def BuildTask(data):
     raw_reply = data.decode()
     reply = json.loads(raw_reply)
@@ -9,9 +10,48 @@ def BuildTask(data):
         replyMsg = str.encode(task)
     else:
         uuid = reply["id"] + 1
-        task = input("Task to complete: ")
-        json_to_load = {"id": uuid, "Type":"cmd", "task": task}
+        Type, cmd, value = TaskType()
+        if (cmd == 0):
+            json_to_load = {"id": uuid, "Type": Type, "task": "admin_tasker"}
+        elif (cmd == 1):
+            json_to_load = {"id": uuid, "Type": Type, "task": "admin_tasker", "value": value}
+        else:
+            json_to_load = {"id": uuid, "Type": Type, "task": cmd}
         json_final = json.dumps(json_to_load)
         replyMsg = str.encode(json_final)
-        
+
     return replyMsg
+
+
+def TaskType():
+    print("What do you want to do?\n")
+    for i in range(0, 2):
+        print("1. Task\n")
+        print("2. Change Callback Time\n")
+        print("3. View Current Callback Time\n")
+
+        choice = int(input("Enter a number: "))
+
+        if (choice == 1):
+            Type = "task"
+            cmd = input("Enter command to task: \n")
+            value = 0
+            return Type, cmd, value
+
+        if (choice == 2):
+            Type = "ChangeTimer"
+            cmd = 1
+            value = int(input("What do you want to change the callback to in hours?\n"))
+            return Type, cmd, value
+ 
+        if (choice == 3):
+            Type = "ViewTimer"
+            cmd = 0
+            value = 0
+            return Type, cmd, value
+
+        else:
+            quit()
+
+    else:
+        quit()
