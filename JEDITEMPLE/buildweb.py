641aa96c4d80298561cd3a847793478b87cba635
jearvey <jamiearvey@yahoo.com>
2020-09-22 19:12:43 -0400

    finalized SQLite database w/ relational tables. Need to actually make it into a build file

--- JEDITEMPLE/buildweb.py
+++ JEDITEMPLE/buildweb.py
@@ -1,7 +1,36 @@
-from flask import Flask
+CREATE TABLE agents(
+  agent_id VARCHAR(26) PRIMARY KEY,
+  callback_id INTEGER NOT NULL,
+  agent_name VARCHAR NOT NULL,
+  os VARCHAR NULL,
+  socket VARCHAR NOT NULL,
+  status VARCHAR NULL 
+);
 
-app = Flask(__name__)
-@app.route('/')
- 
-def hello_world():
-    return "Hello Worl... how original"
+CREATE TABLE agent_callback(
+  id VARCHAR PRIMARY KEY,
+  callback_id INTEGER NOT NULL,
+  last_callback VARCHAR NOT NULL,
+  next_callback VARCHAR NULL,
+  callback_timer INTEGER NULL,
+  FOREIGN KEY (callback_id) REFERENCES agents(callback_id)
+);
+
+CREATE TABLE agent_data(
+  data_id INTEGER PRIMARY KEY,
+  agent_id VARCHAR(26) NOT NULL,
+  cmd_tasked VARCHAR NOT NULL,
+  data_returned VARCHAR NULL,
+  date_time TEXT NOT NULL,
+  FOREIGN KEY (agent_id) REFERENCES agents(agent_id)
+);
+
+CREATE TABLE agent_task(
+  task_id INTEGER PRIMARY KEY,
+  agent_id VARCHAR(26) NOT NULL,
+  type VARCHAR NULL,
+  value VARCHAR NULL,
+  cmd VARCHAR NOT NULL,
+  tasked_time TEXT NOT NULL,
+  FOREIGN KEY (agent_id) REFERENCES agents(agent_id)
+);
