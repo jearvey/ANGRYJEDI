d67adc912bf2908095ff613082893b85e18c7650
jearvey <jamiearvey@yahoo.com>
2020-09-22 20:12:01 -0400

    linked sqlite3 and flask backend with admin functions as well. Need to build out front end

--- JEDITEMPLE/buildweb.py
+++ JEDITEMPLE/buildweb.py
@@ -1,7 +1,46 @@
+import sqlite3
 from flask import Flask
 
+
+###Links database and creates function to make db
+DATABASE = '/root/ANGRYJEDI/JEDIARCHIVES/ANJE_db.sqlite'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+#returns dics instead of tuples from db
+def make_dicts(cursor, row):
+    return dict((cursor.description[idx][0], value)
+                for idx, value in enumerate(row))
+
+db.row_factory = make_dicts
+
+#easy db query, auto open and close
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
+
+###end DB section
+
+
+###Flask Code
 app = Flask(__name__)
 @app.route('/')
 
 def hello_world():
     return "Hello Worl... how original"
+
+
+@app.teardown_appxontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+###end flask code
